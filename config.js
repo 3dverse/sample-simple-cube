ccc099974ba95808197eac93acc30624e87e4dc3
Mark Downey <mark@3dverse.com>
2024-02-16 18:51:43 -0500

    add demo config

--- config.js
+++ config.js
@@ -2,11 +2,11 @@
 // - Go to the API Access page.
 // - Copy the Public Token and paste it below.
 
-export const publicToken = %YOUR_PUBLIC_TOKEN%;
+export const publicToken = 'public_UjENe9mA-wgTBYvw';
 
 // Replace asset UUIDs:
 // - Go to the Asset Browser page.
 // - Double click on the Public folder to open it.
 // - Select the Main Scene and copy the Asset UUID to replace it below.
 
-export const mainSceneUUID = %YOUR_MAIN_SCENE_UUID%;
+export const mainSceneUUID = '322c6bf7-52eb-4197-ab42-a0924f71d72d';
